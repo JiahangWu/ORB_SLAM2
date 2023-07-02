bb9d1e08d6fbab13e63e03d98d38fb8c8fba3352
Ryan <wujiahang.ai@gmail.com>
2023-07-02 22:56:34 +0800

    first commit

---
+++ build/CMakeFiles/ORB_SLAM2.dir/DependInfo.cmake
@@ -0,0 +1,48 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/ryan/Code/ORB_SLAM2/src/Converter.cc" "/home/ryan/Code/ORB_SLAM2/build/CMakeFiles/ORB_SLAM2.dir/src/Converter.cc.o"
+  "/home/ryan/Code/ORB_SLAM2/src/Frame.cc" "/home/ryan/Code/ORB_SLAM2/build/CMakeFiles/ORB_SLAM2.dir/src/Frame.cc.o"
+  "/home/ryan/Code/ORB_SLAM2/src/FrameDrawer.cc" "/home/ryan/Code/ORB_SLAM2/build/CMakeFiles/ORB_SLAM2.dir/src/FrameDrawer.cc.o"
+  "/home/ryan/Code/ORB_SLAM2/src/Initializer.cc" "/home/ryan/Code/ORB_SLAM2/build/CMakeFiles/ORB_SLAM2.dir/src/Initializer.cc.o"
+  "/home/ryan/Code/ORB_SLAM2/src/KeyFrame.cc" "/home/ryan/Code/ORB_SLAM2/build/CMakeFiles/ORB_SLAM2.dir/src/KeyFrame.cc.o"
+  "/home/ryan/Code/ORB_SLAM2/src/KeyFrameDatabase.cc" "/home/ryan/Code/ORB_SLAM2/build/CMakeFiles/ORB_SLAM2.dir/src/KeyFrameDatabase.cc.o"
+  "/home/ryan/Code/ORB_SLAM2/src/LocalMapping.cc" "/home/ryan/Code/ORB_SLAM2/build/CMakeFiles/ORB_SLAM2.dir/src/LocalMapping.cc.o"
+  "/home/ryan/Code/ORB_SLAM2/src/LoopClosing.cc" "/home/ryan/Code/ORB_SLAM2/build/CMakeFiles/ORB_SLAM2.dir/src/LoopClosing.cc.o"
+  "/home/ryan/Code/ORB_SLAM2/src/Map.cc" "/home/ryan/Code/ORB_SLAM2/build/CMakeFiles/ORB_SLAM2.dir/src/Map.cc.o"
+  "/home/ryan/Code/ORB_SLAM2/src/MapDrawer.cc" "/home/ryan/Code/ORB_SLAM2/build/CMakeFiles/ORB_SLAM2.dir/src/MapDrawer.cc.o"
+  "/home/ryan/Code/ORB_SLAM2/src/MapPoint.cc" "/home/ryan/Code/ORB_SLAM2/build/CMakeFiles/ORB_SLAM2.dir/src/MapPoint.cc.o"
+  "/home/ryan/Code/ORB_SLAM2/src/ORBextractor.cc" "/home/ryan/Code/ORB_SLAM2/build/CMakeFiles/ORB_SLAM2.dir/src/ORBextractor.cc.o"
+  "/home/ryan/Code/ORB_SLAM2/src/ORBmatcher.cc" "/home/ryan/Code/ORB_SLAM2/build/CMakeFiles/ORB_SLAM2.dir/src/ORBmatcher.cc.o"
+  "/home/ryan/Code/ORB_SLAM2/src/Optimizer.cc" "/home/ryan/Code/ORB_SLAM2/build/CMakeFiles/ORB_SLAM2.dir/src/Optimizer.cc.o"
+  "/home/ryan/Code/ORB_SLAM2/src/PnPsolver.cc" "/home/ryan/Code/ORB_SLAM2/build/CMakeFiles/ORB_SLAM2.dir/src/PnPsolver.cc.o"
+  "/home/ryan/Code/ORB_SLAM2/src/Sim3Solver.cc" "/home/ryan/Code/ORB_SLAM2/build/CMakeFiles/ORB_SLAM2.dir/src/Sim3Solver.cc.o"
+  "/home/ryan/Code/ORB_SLAM2/src/System.cc" "/home/ryan/Code/ORB_SLAM2/build/CMakeFiles/ORB_SLAM2.dir/src/System.cc.o"
+  "/home/ryan/Code/ORB_SLAM2/src/Tracking.cc" "/home/ryan/Code/ORB_SLAM2/build/CMakeFiles/ORB_SLAM2.dir/src/Tracking.cc.o"
+  "/home/ryan/Code/ORB_SLAM2/src/Viewer.cc" "/home/ryan/Code/ORB_SLAM2/build/CMakeFiles/ORB_SLAM2.dir/src/Viewer.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "COMPILEDWITHC11"
+  "ORB_SLAM2_EXPORTS"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../"
+  "../include"
+  "/usr/local/include/eigen3"
+  "/usr/local/include/opencv"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
